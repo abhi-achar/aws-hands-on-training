042ca0e827ae6c3978ac6e32278a5abbb30912ee
abhi-achar <abhirvce2@gmail.com>
2026-06-21 12:32:47 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""CDK App entry point."""
+
+import aws_cdk as cdk
+
+from cdk.order_processing_stack import OrderProcessingStack
+
+app = cdk.App()
+
+OrderProcessingStack(
+    app,
+    "OrderProcessingStack",
+    env=cdk.Environment(
+        account="353211646521",
+        region="ap-south-1",
+    ),
+)
+
+app.synth()
